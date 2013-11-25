f2b1f90bd3a530200874b6748f07559174193874
Ryan C. Thompson <rct+cpan@thompsonclan.org>
2013-11-25 06:13:55 +0000

    Import of RTHOMPSON/Dist-Zilla-Plugin-ReadmeAnyFromPod-0.133290 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-ReadmeAnyFromPod
    gitpan-cpan-version:      0.133290
    gitpan-cpan-path:         RTHOMPSON/Dist-Zilla-Plugin-ReadmeAnyFromPod-0.133290.tar.gz
    gitpan-cpan-author:       RTHOMPSON
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/ReadmeAnyFromPod.pm
+++ lib/Dist/Zilla/Plugin/ReadmeAnyFromPod.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::ReadmeAnyFromPod;
 {
-  $Dist::Zilla::Plugin::ReadmeAnyFromPod::VERSION = '0.133060'; # TRIAL
+  $Dist::Zilla::Plugin::ReadmeAnyFromPod::VERSION = '0.133290';
 }
 # ABSTRACT: Automatically convert POD to a README in any format for Dist::Zilla
 
@@ -306,7 +306,7 @@ Dist::Zilla::Plugin::ReadmeAnyFromPod - Automatically convert POD to a README in
 
 =head1 VERSION
 
-version 0.133060
+version 0.133290
 
 =head1 SYNOPSIS
 
